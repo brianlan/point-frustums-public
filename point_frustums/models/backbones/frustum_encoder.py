brianlan/point-frustums-public

commit af4a959036a69f6fb93431a4935613fb44aa104e

Make an effort towards the model being scriptable (RecursiveScriptModule and control flow issues remained)

point_frustums/models/backbones/frustum_encoder.py
@@ -336,7 +336,7 @@ class FrustumEncoder(nn.Module):  # pylint: disable=too-many-instance-attributes
 
     def __init__(  # pylint: disable=too-many-arguments
         self,
-        channels_in: tuple[str, ...],
+        channels_in: list[str, ...],
         discretize: ConfigDiscretize,
         decorate: ConfigDecorate,
         vectorize: ConfigVectorize,
@@ -406,7 +406,7 @@ class FrustumEncoder(nn.Module):  # pylint: disable=too-many-instance-attributes
                 channels.append(self.channels_in.index(channel))
         return channels
 
-    def filter_fov(self, pc):
+    def filter_fov(self, pc: torch.Tensor) -> torch.Tensor:
         channel_index_azi, channel_index_pol = self.channels_in.index("azimuthal"), self.channels_in.index("polar")
         mask = (
             (pc[:, channel_index_azi] >= self.discretize.fov_azi[0])
@@ -414,7 +414,7 @@ class FrustumEncoder(nn.Module):  # pylint: disable=too-many-instance-attributes
             & (pc[:, channel_index_pol] >= self.discretize.fov_pol[0])
             & (pc[:, channel_index_pol] < self.discretize.fov_pol[1])
         )
-        return pc[mask]
+        return pc[mask, ...]
 
     def get_frustum_index(self, pc: torch.Tensor):
         channel_index_azi, channel_index_pol = self.channels_in.index("azimuthal"), self.channels_in.index("polar")
@@ -500,7 +500,7 @@ class FrustumEncoder(nn.Module):  # pylint: disable=too-many-instance-attributes
             featuremap.extend(
                 self.apply_symmetric_functions(
                     pc=self.ffn_vectorize(pc),
-                    n_frustums=self.discretize.n_splits,
+                    n_frustums=self.n_splits,
                     i_frustum=i_frustum,
                     i_inv=i_inv,
                     counts_padded=counts_padded,
@@ -512,7 +512,7 @@ class FrustumEncoder(nn.Module):  # pylint: disable=too-many-instance-attributes
 
         return torch.cat(featuremap, dim=1)
 
-    def forward(self, batch: Sequence[torch.Tensor]) -> torch.Tensor:
+    def forward(self, batch: list[torch.Tensor]) -> torch.Tensor:
         batch_size = len(batch)
         features = batch[0].new_empty((batch_size, self.discretize.n_splits, self.n_channels["squashed"]))
 

point_frustums/models/point_frustums.py
@@ -1,8 +1,9 @@
 from typing import Literal, Optional
 from math import prod, exp, isclose
+from copy import deepcopy
 
 import torch
-from torch import nn
+from torch import nn, optim
 from torch.nn import functional as F
 from torchvision.ops import sigmoid_focal_loss
 
@@ -31,10 +32,7 @@ from .training.target_assignment import match_target_projection_to_receptive_fie
 
 class PointFrustumsModel(Detection3DModel):
     def __init__(self, backbone: PointFrustumsBackbone, neck: PointFrustumsNeck, head: PointFrustumsHead):
-        super().__init__()
-        self.backbone = backbone
-        self.neck = neck
-        self.head = head
+        super().__init__(backbone, neck, head)
 
 
 class PointFrustums(Detection3DRuntime):  # pylint: disable=too-many-ancestors
@@ -48,6 +46,10 @@ class PointFrustums(Detection3DRuntime):  # pylint: disable=too-many-ancestors
         **kwargs,
     ):
         super().__init__(*args, model=model, dataset=dataset, **kwargs)
+        self.discretization = deepcopy(self.model.backbone.lidar.discretize)
+        self.strides = deepcopy(self.model.neck.fpn.strides)
+        self.fpn_layers = deepcopy(self.model.neck.fpn.layers)
+        self.fpn_extra_layers = deepcopy(self.model.neck.fpn.extra_layers)
         self.target_assignment = target_assignment
         self.losses = losses
         # TODO: Add postprocessing step
@@ -88,7 +90,7 @@ class PointFrustums(Detection3DRuntime):  # pylint: disable=too-many-ancestors
         _strides_pol_prev = []
         _strides_azi_prev = []
 
-        for layer, specs in self.model.neck.fpn.layers.items():
+        for layer, specs in self.fpn_layers.items():
             layers.append(layer)
             kernels[layer] = _kernels_prev + specs.n_blocks * [3]
             layer_stride_pol, layer_stride_azi = specs.stride
@@ -99,7 +101,7 @@ class PointFrustums(Detection3DRuntime):  # pylint: disable=too-many-ancestors
             _strides_pol_prev = strides_pol[layer]
             _strides_azi_prev = strides_azi[layer]
 
-        for extra_layer, specs in self.model.neck.fpn.extra_layers.items():
+        for extra_layer, specs in self.fpn_extra_layers.items():
             layers.append(extra_layer)
             kernels[extra_layer] = _kernels_prev + [3]
             strides_pol[extra_layer] = _strides_pol_prev + [specs.stride[0]]
@@ -146,13 +148,12 @@ class PointFrustums(Detection3DRuntime):  # pylint: disable=too-many-ancestors
         layers_rfs = list(zip(layers_rfs_pol.values(), layers_rfs_azi.values()))
         reference_boxes_centered = 0.5 * torch.tensor(layers_rfs).repeat(1, 2) * torch.tensor([-1, -1, 1, 1])
 
-        discretize = self.model.backbone.lidar.discretize
-        strides = self.model.neck.fpn.strides.values()
+        discretize = self.discretization
 
         size_per_layer = []
         size_per_layer_flat = []
         feat_rfs_center = []
-        for layer, stride in self.model.neck.fpn.strides.items():
+        for layer, stride in self.strides.items():
             assert isclose(discretize.n_splits_pol % stride[0], 0), (
                 f"The polar input featuremap resolution {discretize.n_splits_pol} is not divisible by the stride "
                 f"{stride[0]} on layer {layer}."
@@ -192,7 +193,7 @@ class PointFrustums(Detection3DRuntime):  # pylint: disable=too-many-ancestors
         self.register_buffer("feat_center_pol_azi", feat_center_pol_azi)
 
         return ModelOutputSpecification(
-            strides=list(strides),
+            strides=list(self.strides.values()),
             layer_sizes=size_per_layer,
             layer_sizes_flat=size_per_layer_flat,
             total_size=sum(size_per_layer_flat),
@@ -235,8 +236,8 @@ class PointFrustums(Detection3DRuntime):  # pylint: disable=too-many-ancestors
             center_pol_azi = center_pol_azi[idx_feat, :]
         x = x.clone()
         x[..., [1, 2]] -= center_pol_azi
-        x[..., 1] /= self.model.backbone.lidar.discretize.delta_pol
-        x[..., 2] /= self.model.backbone.lidar.discretize.delta_azi
+        x[..., 1] /= self.discretization.delta_pol
+        x[..., 2] /= self.discretization.delta_azi
         return x
 
     def _decode_center(self, x: torch.Tensor, idx_feat: Optional[torch.Tensor] = None) -> torch.Tensor:
@@ -251,8 +252,8 @@ class PointFrustums(Detection3DRuntime):  # pylint: disable=too-many-ancestors
             x = x[..., idx_feat, :]
             center_pol_azi = center_pol_azi[idx_feat, :]
         x = x.clone()
-        x[..., 1] *= self.model.backbone.lidar.discretize.delta_pol
-        x[..., 2] *= self.model.backbone.lidar.discretize.delta_azi
+        x[..., 1] *= self.discretization.delta_pol
+        x[..., 2] *= self.discretization.delta_azi
         x[..., [1, 2]] += center_pol_azi
         return x
 
@@ -372,7 +373,7 @@ class PointFrustums(Detection3DRuntime):  # pylint: disable=too-many-ancestors
             rf_centers=self.receptive_fields_centers,
             rf_sizes=self.receptive_fields_sizes,
             layer_sizes_flat=self.featuremap_parametrization.layer_sizes_flat,
-            base_featuremap_width=self.model.backbone.lidar.discretize.n_splits_azi,
+            base_featuremap_width=self.discretization.n_splits_azi,
         )
 
         # Get the indices that map between feature vectors and targets
@@ -552,12 +553,12 @@ class PointFrustums(Detection3DRuntime):  # pylint: disable=too-many-ancestors
     def get_losses(
         self,
         predictions: dict[
-            Literal["class", "attribute", "center", "wlh", "orientation", "velocity", "iou"], dict[str, torch.Tensor]
+            Literal["class", "attribute", "center", "wlh", "orientation", "velocity", "vfl"], dict[str, torch.Tensor]
         ],
         targets: list[Targets],
         ego_velocities: list[torch.Tensor],
     ) -> dict[
-        Literal["class", "attribute", "iou", "center_radial", "center_angular", "wlh", "orientation", "velocity"],
+        Literal["class", "attribute", "vfl", "center_radial", "center_angular", "wlh", "orientation", "velocity"],
         torch.Tensor,
     ]:
         # Flatten the last 2 dimensions of all predictions featuremaps, swap dimensions  and concatenate
@@ -618,7 +619,7 @@ class PointFrustums(Detection3DRuntime):  # pylint: disable=too-many-ancestors
         losses = {
             "class": sigmoid_focal_loss(predictions["class"], targets_label, reduction="sum"),
             "attribute": sigmoid_focal_loss(predictions["attribute"], targets_attribute, reduction="sum"),
-            "vfl": vfl(predictions["iou"], targets_iou, reduction="sum", **self.losses.vfl.kwargs),
+            "vfl": vfl(predictions["vfl"], targets_iou, reduction="sum", **self.losses.vfl.kwargs),
         }
 
         # Evaluate the losses that are applied only to the foreground (center/wlh/orientation/velocity)
@@ -690,5 +691,7 @@ class PointFrustums(Detection3DRuntime):  # pylint: disable=too-many-ancestors
     def test_step(self, batch, batch_idx):
         pass
 
-    def configure_optimizers(self):
-        pass
+    def configure_optimizers(self, *args, **kwargs):
+        optimizer = optim.AdamW(self.parameters())
+        # lr_scheduler = optim.lr_scheduler.OneCycleLR(optimizer)
+        return [optimizer], []

point_frustums/models/backbones/__init__.py
@@ -10,7 +10,7 @@ class PointFrustumsBackbone(Backbone):
         self.camera = camera
         self.radar = radar
 
-    def forward_lidar(self, lidar: Tensor) -> Tensor:
+    def forward_lidar(self, lidar: list[Tensor]) -> Tensor:
         return self.lidar(lidar)
 
     def forward_camera(self, camera: Tensor) -> Tensor:

point_frustums/models/base_models.py
@@ -1,5 +1,4 @@
 from typing import Optional
-from collections.abc import Mapping, MutableMapping
 from abc import ABCMeta, abstractmethod
 
 from torch import nn, Tensor
@@ -7,7 +6,7 @@ from torch import nn, Tensor
 
 class Backbone(nn.Module, metaclass=ABCMeta):
     @abstractmethod
-    def forward_lidar(self, lidar: Tensor) -> Tensor:
+    def forward_lidar(self, lidar: list[Tensor]) -> Tensor:
         pass
 
     @abstractmethod
@@ -20,16 +19,11 @@ class Backbone(nn.Module, metaclass=ABCMeta):
 
     def forward(
         self,
-        lidar: Optional[MutableMapping[str, Tensor]] = None,
-        camera: Optional[MutableMapping[str, Tensor]] = None,
-        radar: Optional[MutableMapping[str, Tensor]] = None,
-    ) -> tuple[
-        Optional[MutableMapping[str, Tensor]],
-        Optional[MutableMapping[str, Tensor]],
-        Optional[MutableMapping[str, Tensor]],
-    ]:
-        if lidar is not None:
-            lidar = {sensor: self.forward_lidar(data) for sensor, data in lidar.items()}
+        lidar: dict[str, list[Tensor]],
+        camera: Optional[dict[str, Tensor]] = None,
+        radar: Optional[dict[str, Tensor]] = None,
+    ) -> tuple[dict[str, Tensor], Optional[dict[str, Tensor]], Optional[dict[str, Tensor]],]:
+        lidar = {sensor: self.forward_lidar(data) for sensor, data in lidar.items()}
 
         if camera is not None:
             camera = {sensor: self.forward_camera(data) for sensor, data in camera.items()}
@@ -44,30 +38,32 @@ class Neck(nn.Module, metaclass=ABCMeta):
     @abstractmethod
     def forward(
         self,
-        lidar: Optional[MutableMapping[str, Tensor]],
-        camera: Optional[MutableMapping[str, Tensor]],
-        radar: Optional[MutableMapping[str, Tensor]],
-    ) -> Tensor | Mapping[str, Tensor]:
+        lidar: dict[str, Tensor],
+        camera: Optional[dict[str, Tensor]],
+        radar: Optional[dict[str, Tensor]],
+    ) -> dict[str, Tensor]:
         pass
 
 
 class Head(nn.Module, metaclass=ABCMeta):
     @abstractmethod
-    def forward(self, features: Tensor | Mapping[str, Tensor]) -> Mapping[str, Tensor]:
+    def forward(self, features: Tensor | dict[str, Tensor]) -> dict[str, Tensor]:
         pass
 
 
 class Detection3DModel(nn.Module):
-    backbone: Backbone
-    neck: Neck
-    head: Head
+    def __init__(self, backbone: Backbone, neck: Neck, head: Head):
+        super().__init__()
+        self.backbone = backbone
+        self.neck = neck
+        self.head = head
 
     def forward(
         self,
-        lidar: Optional[MutableMapping[str, Tensor]] = None,
-        camera: Optional[MutableMapping[str, Tensor]] = None,
-        radar: Optional[MutableMapping[str, Tensor]] = None,
-    ):
+        lidar: dict[str, list[Tensor]],
+        camera: Optional[dict[str, Tensor]] = None,
+        radar: Optional[dict[str, Tensor]] = None,
+    ) -> dict[str, dict[str, Tensor]]:
         lidar, camera, radar = self.backbone(lidar=lidar, camera=camera, radar=radar)
         features = self.neck(lidar=lidar, camera=camera, radar=radar)
         return self.head(features=features)

point_frustums/models/heads/__init__.py
@@ -23,7 +23,7 @@ class PointFrustumsHead(Head):
     ):
         super().__init__()
         self.n_channels_in = n_channels_in
-        self.layers_in = layers_in
+        self.layers_in = tuple(layers_in)
         self.n_convolutions_classification = n_convolutions_classification
         self.n_convolutions_regression = n_convolutions_regression
         self.share_weights = share_weights
@@ -40,7 +40,7 @@ class PointFrustumsHead(Head):
         self.head_classification = self.build_head(self.spec_cls, n_convolutions_classification, share_weights)
         self.head_regression = self.build_head(self.spec_reg, n_convolutions_regression, share_weights)
 
-    def build_head(self, outputs: dict[str, int], n_convolutions: int, share_weights: bool = True):
+    def build_head(self, outputs: dict[str, int], n_convolutions: int, share_weights: bool = True) -> nn.ModuleList:
         """
         Builds a head with several convolutional layers and one final layer that stacks the specified outputs.
         If `share_weights` is False, the built head is duplicated, otherwise it is reused for each layer.
@@ -63,26 +63,31 @@ class PointFrustumsHead(Head):
         layers.append(Conv2dSpherical(self.n_channels_in, n_channels_out, kernel_size=1))
         layers = nn.Sequential(*layers)
 
-        head = nn.ModuleDict()
-        for layer in self.layers_in:
+        head = nn.ModuleList()
+        for _ in self.layers_in:
             if not share_weights:
                 layers = deepcopy(layers)
-            head[layer] = layers
+            head.append(layers)
 
         return head
 
-    def forward(self, features: Mapping[str, Tensor]) -> dict[str, dict[str, Tensor]]:
+    def forward(self, features: dict[str, Tensor]) -> dict[str, dict[str, Tensor]]:
         classification = {}
         regression = {}
-        for layer in self.layers_in:
-            classification[layer] = self.head_classification[layer](features[layer])
-            regression[layer] = self.head_regression[layer](features[layer])
+        for layer, head_cls, head_reg in zip(self.layers_in, self.head_classification, self.head_regression):
+            classification[layer] = head_cls(features[layer])
+            regression[layer] = head_reg(features[layer])
 
         out = {k: {} for k in self.out_keys}
         for layer in self.layers_in:
             cls = classification[layer].split(self.split_sizes_classification, dim=1)
             reg = regression[layer].split(self.split_sizes_regression, dim=1)
-            for head, tensor in zip(self.out_keys, cls + reg):
-                out[head][layer] = tensor
+            out["class"][layer] = cls[0]
+            out["attribute"][layer] = cls[1]
+            out["center"][layer] = reg[0]
+            out["wlh"][layer] = reg[1]
+            out["orientation"][layer] = reg[2]
+            out["velocity"][layer] = reg[3]
+            out["vfl"][layer] = reg[4]
 
         return out

point_frustums/models/necks/__init__.py
@@ -49,10 +49,10 @@ class PointFrustumsNeck(Neck):
 
     def forward(
         self,
-        lidar: MutableMapping[str, Tensor],
-        camera: Optional[MutableMapping[str, Tensor]] = None,
-        radar: Optional[MutableMapping[str, Tensor]] = None,
-    ) -> Tensor | Mapping[str, Tensor]:
+        lidar: dict[str, Tensor],
+        camera: Optional[dict[str, Tensor]] = None,
+        radar: Optional[dict[str, Tensor]] = None,
+    ) -> dict[str, Tensor]:
         featuremap = [lidar[self.target_sensor]]
         # TODO: Implement the mapping in case multiple LiDAR sensors are used
         # TODO: Implement the fusion functions; have to be executed for each sensor

point_frustums/models/necks/spherical_coos_fpn.py
@@ -1,4 +1,3 @@
-from collections.abc import Mapping
 from typing import Optional
 
 import torch
@@ -65,7 +64,7 @@ class FPN(nn.Module):
         self,
         n_channels_in: int,
         n_channels_out: int,
-        layers: Mapping[str, ConfigFPNLayer],
+        layers: dict[str, ConfigFPNLayer],
         extra_layers: Optional[dict[str, ConfigFPNExtraLayer]] = None,
         dropout: float = 0.0,
         upsampling_mode: str = "bilinear",
@@ -76,9 +75,11 @@ class FPN(nn.Module):
         self.dropout = dropout
         self.upsampling_mode = upsampling_mode
         self.layers = dict(sorted(layers.items()))
-        layers_keys = list(layers.keys())
-        self.first_layer, self.last_layer = layers_keys[0], layers_keys[-1]
+        self.names_layers = tuple(self.layers.keys())
+        self.names_layers_reversed = tuple(reversed(self.names_layers))
+        self.first_layer, self.last_layer = self.names_layers[0], self.names_layers[-1]
         self.extra_layers = dict(sorted(extra_layers.items())) if extra_layers is not None else {}
+        self.names_extra_layers = tuple(self.extra_layers.keys())
 
         self.strides = {}
         self.up, self.lateral, self.down, self.post = self.build_layers()
@@ -105,7 +106,7 @@ class FPN(nn.Module):
         )
         return block
 
-    def build_layers(self) -> tuple[nn.ModuleDict, nn.ModuleDict, nn.ModuleDict, nn.ModuleDict]:
+    def build_layers(self) -> tuple[nn.ModuleList, nn.ModuleList, nn.ModuleList, nn.ModuleList]:
         """Build the layers of the FPN.
         A FPN layer consists of several components:
             1. The top-down path is made up of a series of blocks (the first sets the size and dimension for the layer)
@@ -158,51 +159,56 @@ class FPN(nn.Module):
                 )
             prev = name
 
-        return nn.ModuleDict(up), nn.ModuleDict(lateral), nn.ModuleDict(down), nn.ModuleDict(post)
+        up = nn.ModuleList([up[layer] for layer in self.names_layers])
+        lateral = nn.ModuleList([lateral[layer] for layer in self.names_layers])
+        down = nn.ModuleList([down[layer] for layer in self.names_layers_reversed if layer in down])
+        post = nn.ModuleList([post[layer] for layer in self.names_layers_reversed if layer in post])
 
-    def build_extra_layers(self) -> tuple[nn.ModuleDict, nn.ModuleDict]:
+        return up, lateral, down, post
+
+    def build_extra_layers(self) -> tuple[nn.ModuleList, nn.ModuleList]:
         prev = list(self.strides.keys())[-1]
-        extra_layers = {}
-        extra_layers_post = {}
+        extra_layers = nn.ModuleList()
+        extra_layers_post = nn.ModuleList()
         n_channels_in = self.layers[self.last_layer].n_channels * self.block.expansion
         for name, layer in self.extra_layers.items():
             self.strides[name] = (layer.stride[0] * self.strides[prev][0], layer.stride[1] * self.strides[prev][1])
-            extra_layers[name] = nn.Sequential(
-                nn.Dropout(self.dropout),
-                Conv2dSpherical(n_channels_in, n_channels_in, kernel_size=3, stride=layer.stride),
-                self.norm_layer(n_channels_in),
+            extra_layers.append(
+                nn.Sequential(
+                    nn.Dropout(self.dropout),
+                    Conv2dSpherical(n_channels_in, n_channels_in, kernel_size=3, stride=layer.stride),
+                    self.norm_layer(n_channels_in),
+                )
             )
-            extra_layers_post[name] = nn.Sequential(
-                nn.Dropout2d(self.dropout),
-                Conv2dSpherical(n_channels_in, self.n_channels_out, kernel_size=3),
-                self.norm_layer(self.n_channels_out),
+            extra_layers_post.append(
+                nn.Sequential(
+                    nn.Dropout2d(self.dropout),
+                    Conv2dSpherical(n_channels_in, self.n_channels_out, kernel_size=3),
+                    self.norm_layer(self.n_channels_out),
+                )
             )
             prev = name
-        return nn.ModuleDict(extra_layers), nn.ModuleDict(extra_layers_post)
+        return extra_layers, extra_layers_post
 
     def forward(self, input: torch.Tensor) -> dict[str, torch.Tensor]:
         """Forward pass of the FPN"""
         results = {}
 
         # Walk through the bottom-up path and directly apply the lateral FC layer
-        for layer in self.layers.keys():
-            input = self.up[layer](input)
-            results[layer] = self.lateral[layer](input)
+        for layer, up, lateral in zip(self.names_layers, self.up, self.lateral):
+            input = up(input)
+            results[layer] = lateral(input)
 
         # Evaluate extra layers, building on the result of the last bottom-up layer
-        for layer in self.extra_layers.keys():
-            input = self.extra[layer](input)
-            results[layer] = self.extra_post[layer](input)
+        for layer, extra, extra_post in zip(self.names_extra_layers, self.extra, self.extra_post):
+            input = extra(input)
+            results[layer] = extra_post(input)
 
         # Walk through the top-down path, add the up-sampled featuremaps to the intermediate result, apply the FC layer
-        input = None
-        for layer in reversed(self.layers.keys()):
-            if input is None:
-                # The upmost layer is used directly as returned by the bottom-up path
-                input = results[layer]
-                continue
-            results[layer] += self.down[layer](input)
+        input = results[self.names_layers_reversed[0]]
+        for layer, down, post in zip(self.names_layers_reversed[1:], self.down, self.post):
+            results[layer] += down(input)
             input = results[layer]
-            results[layer] = self.post[layer](results[layer])
+            results[layer] = post(results[layer])
 
         return results

commit 55f00fd0958bce21bd89740affe14f0f717d86a4

Bugfixes: wrong property name (introduced in af4a9590) and minimum between tensor and inbuilt numeric.

point_frustums/models/backbones/frustum_encoder.py
@@ -500,7 +500,7 @@ class FrustumEncoder(nn.Module):  # pylint: disable=too-many-instance-attributes
             featuremap.extend(
                 self.apply_symmetric_functions(
                     pc=self.ffn_vectorize(pc),
-                    n_frustums=self.n_splits,
+                    n_frustums=self.discretize.n_splits,
                     i_frustum=i_frustum,
                     i_inv=i_inv,
                     counts_padded=counts_padded,

point_frustums/models/point_frustums.py
@@ -1,6 +1,6 @@
-from typing import Literal, Optional
-from math import prod, exp, isclose
 from copy import deepcopy
+from math import prod, exp, isclose
+from typing import Literal, Optional
 
 import torch
 from torch import nn, optim
@@ -15,13 +15,13 @@ from point_frustums.config_dataclasses.point_frustums import (
     Predictions,
 )
 from point_frustums.geometry.coordinate_system_conversion import sph_to_cart_torch, cart_to_sph_torch
-from point_frustums.geometry.utils import get_corners_3d, get_spherical_projection_boundaries, iou_vol_3d
 from point_frustums.geometry.quaternion import rotate_2d
 from point_frustums.geometry.rotation_matrix import (
     rotation_matrix_from_spherical_coordinates,
     rotation_matrix_to_rotation_6d,
     rotation_matrix_from_rotation_6d,
 )
+from point_frustums.geometry.utils import get_corners_3d, get_spherical_projection_boundaries, iou_vol_3d
 from point_frustums.ops.nms import nms_3d
 from point_frustums.utils.targets import Targets
 from .backbones import PointFrustumsBackbone
@@ -760,7 +760,7 @@ class PointFrustums(Detection3DRuntime):  # pylint: disable=too-many-ancestors
             )
             nms_keep_indices = nms_keep_mask.nonzero().squeeze(dim=1)
             # Subset the duplicate-free outputs to the configured number of detections
-            k = min(nms_keep_mask.count_nonzero(), self.predictions.n_detections)
+            k = nms_keep_mask.count_nonzero().min(torch.tensor(self.predictions.n_detections))
             detections_top_scores, detections_top_indices = sample_top_scores[nms_keep_mask].topk(k)
             detections_top_indices = nms_keep_indices[detections_top_indices]
 