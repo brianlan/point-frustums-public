0aa1c529636972acd1040571296ffb043cfee364
Enrico Stauss <enrico_sta@web.de>
2024-03-17 20:41:10 +0100

    NMS3D CUDA Kernel: Put all box parameters into a struct and map to tensor rows to improve memory access times

---
+++ point_frustums/ops/csrc/nms/nms_utils.cuh
@@ -0,0 +1,18 @@
+//
+// Created by Enrico Stauss on 15.03.24.
+//
+
+#include "iou_box3d/iou_utils.cuh"
+#include "utils/float_math.cuh"
+
+int N_FLOATS_PER_TRIANGLE = NUM_TRIS * 3 * 3;
+int N_FLOATS_PER_PLANE = NUM_PLANES * 4 * 3;
+
+// Here I can assume that the content of the struct will be placed contiguously in memory.
+// If I then call reinterp_cast on a row of a tensor it should work out.
+struct BoxParameters {
+  float3 center;
+  FaceVerts triangles;
+  FaceVerts planes;
+  float volume;
+};
