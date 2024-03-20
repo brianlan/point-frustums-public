42f1a9f1a653b5e1236b905c2c40b670f3bfab33
Enrico Stauss <enrico_sta@web.de>
2024-03-20 12:21:10 +0100

    Add fine-grained download choices

--- scripts/prepare_nuscenes_dataset.py
+++ scripts/prepare_nuscenes_dataset.py
@@ -12,7 +12,7 @@ def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         prog="Prepare NuScenes Dataset", description="Download and extract all archives specified in the provided file."
     )
-    parser.add_argument("--filename", type=str, default="./urls.txt")
+    parser.add_argument("--filename", type=str, default="./nuscenes_source/urls_complete.txt")
     parser.add_argument(
         "--out_dir", type=str, default=os.path.join(os.path.expandvars("$HOME"), "datasets", "nuscenes")
     )
@@ -29,10 +29,16 @@ def main():
             url = url.strip()
             blob_name = url.rsplit("/")[-1]
             archive_local = os.path.join(downloads_local, blob_name)
-            logfile = os.path.join(downloads_local, "logs", blob_name.rstrip(".tgz") + ".log")
+            blob_base_name, blob_extension = os.path.splitext(blob_name)
+            assert blob_extension in (".tgz", ".zip"), f"Cannot unpack '{blob_extension}' files."
+            logfile = os.path.join(downloads_local, "logs", blob_base_name + ".log")
             cmd_download = f"wget -q -O {archive_local} '{url}'"
-            cmd_unpack = f"tar zxvf '{archive_local}' --skip-old-files --directory {args.out_dir}"
-            cmd = cmd_download + "|" + cmd_unpack + ">" + logfile
+            if blob_extension == ".tgz":
+                cmd_unpack = f"tar zxf '{archive_local}' --skip-old-files --directory {args.out_dir}"
+            elif blob_extension == ".zip":
+                cmd_unpack = f"unzip -n '{archive_local}' -d {args.out_dir}"
+            # I'd use && to chain the commands but wget returns a non-zero exit code when skipping an existing file
+            cmd = cmd_download + "; " + cmd_unpack + ">" + logfile
             proc = Popen(cmd, shell=True)  # pylint: disable=consider-using-with
             logger.info(f"Spawned a subprocess with pid={proc.pid} to download and extract '{blob_name}'")
 
