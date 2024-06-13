Fix some type hints and implicit broadcasting from the left.

point_frustums/geometry/rotation_conversion.py
@@ -217,8 +217,9 @@ def _axis_angle_to_rotation_matrix(axis, angle) -> torch.Tensor:
     """
     Use the Rodrigues-formula [1] to construct `n` random rotation matrices.
     [1]: Q(d, theta) = I + sin(theta) D + (1-cos(theta))D^2  with D = -d @ levi and D^2 = D @ D
-    :return:
+    :return: tensor([N, 3, 3])
     """
     dyad = -torch.einsum("ij,jkl->ikl", axis, levi_civita_symbol(device=axis.device))
     dyad_square = torch.einsum("ijk,ikl->ijl", dyad, dyad)
+    angle = angle.reshape(angle.numel(), *[1 for _ in dyad.shape[1:]])
     return torch.eye(3, device=axis.device)[None, ...] + angle.sin() * dyad + (1 - angle.cos()) * dyad_square

point_frustums/models/point_frustums.py
@@ -624,7 +624,7 @@ class PointFrustums(Detection3DRuntime):  # pylint: disable=too-many-ancestors
     ) -> dict[Literal["velocity"], torch.Tensor]:
         targets_velocity_fg = self._broadcast_targets(targets_velocity, targets_indices_fg)
 
-        # Broadcast the ego velocities to match the foreground vector
+        # Broadcast the ego velocity per sample to match the foreground vector
         ego_velocities = [v[None, :].repeat(i.numel(), 1) for i, v in zip(targets_indices_fg, ego_velocities)]
         ego_velocities = torch.cat(ego_velocities, dim=0)
 