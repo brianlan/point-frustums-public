ec246c7dfd722d594ef227d694f1d92c7fcf400f
Enrico Stauss <enrico_sta@web.de>
2024-09-01 22:42:31 +0200

    Add default values for optional args

--- point_frustums/models/base_runtime.py
+++ point_frustums/models/base_runtime.py
@@ -10,14 +10,21 @@ from point_frustums.models.base_models import Detection3DModel
 
 class Detection3DRuntime(LightningModule, metaclass=ABCMeta):
     def __init__(self, *args, model: Detection3DModel, **kwargs):
+        """
+        The base class for a pytorch lightning module that will serve as runtime for the pytorch model.
+
+        :param args:
+        :param model:
+        :param kwargs:
+        """
         super().__init__(*args, **kwargs)
         self.model = model
 
     def forward(  # pylint: disable=arguments-differ
         self,
         lidar: dict[str, list[Tensor]],
-        camera: Optional[MutableMapping[str, Tensor]],
-        radar: Optional[MutableMapping[str, Tensor]],
+        camera: Optional[MutableMapping[str, Tensor]] = None,
+        radar: Optional[MutableMapping[str, Tensor]] = None,
     ):
         return self.model.forward(lidar=lidar, camera=camera, radar=radar)
 
