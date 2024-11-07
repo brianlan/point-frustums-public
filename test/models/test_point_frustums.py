3f38df36167cb2a155dee6564782723b15232009
Enrico Stauss <enrico_sta@web.de>
2024-11-07 23:52:46 +0100

    Add a unit test for the center loss components

--- test/models/test_point_frustums.py
+++ test/models/test_point_frustums.py
@@ -1,8 +1,15 @@
+from functools import partial
+from math import pi
 from unittest.mock import Mock
+
 import torch
-from point_frustums.models.point_frustums import PointFrustums
+
+from point_frustums.config_dataclasses.fpn import ConfigFPNLayer
+from point_frustums.config_dataclasses.point_frustums import ConfigDiscretize
+from point_frustums.geometry.coordinate_system_conversion import cart_to_sph_torch, sph_to_cart_torch
 from point_frustums.geometry.rotation_matrix import random_rotation_matrix
-from point_frustums.geometry.coordinate_system_conversion import cart_to_sph_torch
+from point_frustums.models.point_frustums import PointFrustums
+
 
 # pylint: disable=protected-access
 
@@ -39,3 +46,95 @@ class TestPointFrustumsCoders:
         wlh_encoded = PointFrustums._encode_wlh(wlh)
         wlh_decoded = PointFrustums._decode_wlh(wlh_encoded)
         assert torch.allclose(wlh, wlh_decoded, atol=1e-6)
+
+
+class TestPointFrustumsLosses:
+    # Instantiate the mocking object
+    module_mock = Mock()
+    # Define the config to use for the mocked PointFrustums model
+    #   -> 2 layers with 2 blocks each and downsampling in both directions
+    #   -> Heads with 4 convolutional layers
+    #   -> Discretization azimuthal [-180, 180]: 8 splits and polar [60, 100]: 4 splits
+    #   -> A stride of 2 for the first layer and 4 for the second layer
+    layer_config_kwargs = {"n_channels": 1, "downsampling_horizontal": True, "downsampling_vertical": True}
+    module_mock.fpn_layers = {
+        "l0": ConfigFPNLayer(n_blocks=2, **layer_config_kwargs),
+        "l1": ConfigFPNLayer(n_blocks=2, **layer_config_kwargs),
+    }
+    module_mock.fpn_extra_layers = {}
+    module_mock.model.head.n_convolutions_classification = 4
+    module_mock.model.head.n_convolutions_regression = 4
+    module_mock.discretization = ConfigDiscretize(
+        n_splits_pol=4, n_splits_azi=8, fov_azi_deg=(-180, 180), fov_pol_deg=(60, 100)
+    )
+    module_mock.strides = {"l0": (2, 2), "l1": (4, 4)}
+
+    # Patch the properties to access the angular receptive field center coordinates
+    module_mock._evaluate_receptive_fields = partial(PointFrustums._evaluate_receptive_fields, self=module_mock)
+    module_mock.register_buffer = partial(setattr, module_mock)
+    fm_params = PointFrustums._register_featuremap_parametrization(module_mock)
+    module_mock.feature_vectors_angular_centers = getattr(module_mock, "feat_center_pol_azi")
+
+    layer_sizes = fm_params.layer_sizes_flat
+    angular_centers = module_mock.feature_vectors_angular_centers.split_with_sizes(layer_sizes, dim=0)
+
+    module_mock._broadcast_targets = PointFrustums._broadcast_targets
+    module_mock._encode_center = partial(PointFrustums._encode_center, module_mock)
+    module_mock.losses.center_radial.kwargs = {}
+    module_mock.losses.center_polar.kwargs = {}
+    module_mock.losses.center_azimuthal.kwargs = {}
+
+    def test_center_los(self):
+        """
+        Test that the center loss equals zero for a well-defined case of 3 targets, located in the upper-left and
+        lower-right frustum of the PointFrustums mock.
+        :return:
+        """
+        # Set the foreground_idx to decide where predictions were made
+        #   -> the first index corresponds to the upper left of the pseudo image
+        #   -> The angular coordinate with the discretization of 4 azimuthal and 2 polar splits (after a stride 2
+        #      downsampling on the first layer) equals to an azimuthal position of [-3/4 pi, 70°].
+        #   -> The angular coordinates with the discretization of 2 azimuthal and 1 polar split (after a stride 4
+        #      downsampling on the second layer) equals to an azimuthal position of [-1/2 pi, 80°].
+        #   -> The first layer outputs 8 predictions 4x2, so the first index of the second layer output equals 8.
+        foreground_idx = torch.tensor([0, 7, 8], dtype=torch.long)
+        # Create some sample predictions (relative to the frustum center) in the upper-left frustum for layer l0 and l1
+        predictions = torch.tensor([[10, 0, 0], [5, 0, 0], [10, 0, 0]], dtype=torch.float)
+        # Create some sample targets (in absolute, cartesian coordinates)
+        targets = sph_to_cart_torch(
+            torch.tensor(
+                [
+                    [10, torch.deg2rad(torch.tensor(70)), -3 / 4 * pi],  # upper-left (l0)
+                    [10, torch.deg2rad(torch.tensor(80)), -1 / 2 * pi],  # upper-left (l1)
+                    [5, torch.deg2rad(torch.tensor(90)), 3 / 4 * pi],  # lower-right (l0)
+                ],
+                dtype=torch.float,
+            )
+        )
+        # Set the mapping from target indices to foreground predictions
+        targets_indices_foreground = torch.tensor([0, 2, 1], dtype=torch.long)
+        # Compute the loss
+        losses = PointFrustums._loss_center(
+            self.module_mock,
+            predictions_center_fg=predictions,
+            foreground_idx=foreground_idx,
+            targets_center=[targets],
+            targets_indices_fg=[targets_indices_foreground],
+        )
+        zero = torch.tensor(0.0)
+        assert torch.allclose(losses["center_radial"], zero)
+        assert torch.allclose(losses["center_polar"], zero)
+        assert torch.allclose(losses["center_azimuthal"], zero)
+
+        # Second case, the predictions were inaccurate, we'd expect all losses to be nozero
+        predictions = torch.tensor([[11, 0.5, 0], [4, 0, 0.2], [10, 1, 0]], dtype=torch.float)
+        losses = PointFrustums._loss_center(
+            self.module_mock,
+            predictions_center_fg=predictions,
+            foreground_idx=foreground_idx,
+            targets_center=[targets],
+            targets_indices_fg=[targets_indices_foreground],
+        )
+        assert not torch.isclose(losses["center_radial"], zero).any()
+        assert not torch.isclose(losses["center_polar"], zero).any()
+        assert not torch.isclose(losses["center_azimuthal"], zero).any()
